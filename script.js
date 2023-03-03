c449a22d78639ba17ffa432ad145522a634fe525
Sphik <123711881+Sphik@users.noreply.github.com>
2023-03-02 21:16:32 -0700

    Added weather conditions and wind speed

--- script.js
+++ script.js
@@ -29,13 +29,26 @@ currentDate.innerHTML = formatDate(currentTime);
 let searchForm = document.querySelector("#search-input");
 searchForm.addEventListener("submit", searchCity);
 
+function getForecast(coordinates) {
+  let apiKey = "2ff29bed3181c3526c35cc5408037f85";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function showTemperature(response) {
   let temp = Math.round(response.data.main.temp);
   let displayTemp = document.querySelector("#temperatureNow");
-  displayTemp.innerHTML = `${temp}°C`;
   let changeCity = document.querySelector("#newCity");
-  changeCity.innerHTML = response.data.name;
-}
+   let descriptionItem = document.querySelector("#description");
+   let windSpeed = document.querySelector("#wind");
+
+
+changeCity.innerHTML = response.data.name; 
+displayTemp.innerHTML = `${temp}°C`;
+descriptionItem.innerHTML = response.data.weather[0].description;
+windSpeed.innerHTML = Math.round(response.data.wind.speed * 3.6);
+
+  }
 
 function searchCity(event) {
   event.preventDefault();
